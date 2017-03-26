a62df8c32ce64d7ddee423d58cbb9188eb64bdc8
Ara Kim <ahrakim44@gmail.com>
2017-03-26 09:21:04 -0500

    AJAX call added

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+const authRoutes = require('./server/routes/auth');
+app.use('/auth', authRoutes);
 
 
 app.get('/', (req, res) => {
