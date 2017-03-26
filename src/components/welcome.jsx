caa73efa02d5d631e71e778f2d1102dc5338b22d
Ara Kim <ahrakim44@gmail.com>
2017-03-25 19:11:20 -0500

    adding some folders and components. it's not working yet

---
+++ src/components/welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Card, CardTitle } from 'material-ui/Card';
+
+
+const Welcome = () => (
+  <Card className="container">
+    <CardTitle title="React Application" subtitle="Welcome To SAAS RESUME." />
+  </Card>
+);
+
+export default Welcome;
