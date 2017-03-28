11d541198457e3fa86a85e945cf4f94bd58a8a29
Joy Holowicki <joyholowicki@gmail.com>
2017-03-28 10:15:55 -0500

    Layout of Resume.jsx

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,15 +1,11 @@
 //not sure how i want to handle this yet but i like the paper look and there needs to be a toolbar.
 
-
-///theres a lot of bs in here.
 import React from 'react';
 import IconMenu from 'material-ui/IconMenu';
 import IconButton from 'material-ui/IconButton';
 import FontIcon from 'material-ui/FontIcon';
-import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
 import MenuItem from 'material-ui/MenuItem';
 import DropDownMenu from 'material-ui/DropDownMenu';
-import RaisedButton from 'material-ui/RaisedButton';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 
 export default class ToolbarExamplesSimple extends React.Component {
@@ -24,61 +20,25 @@ export default class ToolbarExamplesSimple extends React.Component {
   handleChange = (event, index, value) => this.setState({value});
 
   render() {
-    return (
+      return (
+          //what do we have to do to get this to work?
+        if () {
+          <div className="warning">
+              Warning!
+    </div>
+        }
       <Toolbar>
         <ToolbarGroup firstChild={true}>
           <DropDownMenu value={this.state.value} onChange={this.handleChange}>
-            <MenuItem value={1} primaryText="All Broadcasts" />
-            <MenuItem value={2} primaryText="All Voice" />
-            <MenuItem value={3} primaryText="All Text" />
-            <MenuItem value={4} primaryText="Complete Voice" />
-            <MenuItem value={5} primaryText="Complete Text" />
-            <MenuItem value={6} primaryText="Active Voice" />
-            <MenuItem value={7} primaryText="Active Text" />
+            <i class="material-icons">text_format</i>
+            <MenuItem value={1} primaryText="Raleway" />
+            <MenuItem value={2} primaryText="Ubuntu" />
+            <MenuItem value={3} primaryText="Open Sans" />
           </DropDownMenu>
         </ToolbarGroup>
-        <ToolbarGroup>
-          <ToolbarTitle text="Options" />
-          <FontIcon className="muidocs-icon-custom-sort" />
-          <ToolbarSeparator />
-          <RaisedButton label="Create Broadcast" primary={true} />
-          <IconMenu
-            iconButtonElement={
-              <IconButton touch={true}>
-                <NavigationExpandMoreIcon />
-              </IconButton>
+       
             }
-          >
-            <MenuItem primaryText="Download" />
-            <MenuItem primaryText="More Info" />
-          </IconMenu>
-        </ToolbarGroup>
       </Toolbar>
     );
   }
 }
-
-
-//////
-import React from 'react';
-import Paper from 'material-ui/Paper';
-
-const style = {
-  height: 100,
-  width: 100,
-  margin: 20,
-  textAlign: 'center',
-  display: 'inline-block',
-};
-
-const PaperExampleRounded = () => (
-  <div>
-    <Paper style={style} zDepth={1} rounded={false} />
-    <Paper style={style} zDepth={2} rounded={false} />
-    <Paper style={style} zDepth={3} rounded={false} />
-    <Paper style={style} zDepth={4} rounded={false} />
-    <Paper style={style} zDepth={5} rounded={false} />
-  </div>
-);
-
-export default PaperExampleRounded;
